d2ed8f11212910131f0bcaef36d2ab514b7dbb75
yboniecabayasomogod <ybonie.cabaya.somogod@gmail.com>
2024-04-27 21:05:05 +0800

    Adding a JSDocs independicies Adding routes add Navbar

---
+++ src/assets/Information.js/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../elements/Navbar'
+
+function About() {
+  return (
+    <Navbar>
+      
+    </Navbar>
+  )
+}
+
+export default About
---
+++ src/assets/Information.js/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../elements/Navbar';
+
+function Home() {
+  return (
+    <div>
+      <Navbar>
+
+      </Navbar>
+    </div>
+  )
+}
+
+export default Home
